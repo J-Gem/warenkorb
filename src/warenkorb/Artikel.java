f6dbac7962ef69b668555f29dc4f2184c5525131
billgeiz <billgeiz@freenet.de>
2026-08-06 12:11:17 +0200

    Implementierung der Artikel Klasse

--- src/warenkorb/Artikel.java
+++ src/warenkorb/Artikel.java
@@ -1,4 +1,48 @@
 package warenkorb;
+
 public class Artikel {
-    
+
+    private int artikelnummer;
+    private String beschreibung;
+    private float nettopreis;
+    private float steuersatz;
+
+    public Artikel(int artikelnummer, String beschreibung, float nettopreis, float steuersatz) {
+        this.artikelnummer = artikelnummer;
+        this.beschreibung = beschreibung;
+        this.nettopreis = nettopreis;
+        this.steuersatz = steuersatz;
+    }
+
+    public void setArtikelnummer(int artikelnummer) {
+        this.artikelnummer = artikelnummer;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public void setNettopreis(float nettopreis) {
+        this.nettopreis = nettopreis;
+    }
+
+    public void setSteuersatz(float steuersatz) {
+        this.steuersatz = steuersatz;
+    }
+
+    public int getArtikelnummer() {
+        return this.artikelnummer;
+    }
+
+    public String getBeschreibung() {
+        return this.beschreibung;
+    }
+
+    public float getNettopreis() {
+        return this.nettopreis;
+    }
+
+    public float getSteuersatz() {
+        return this.steuersatz;
+    }
 }
